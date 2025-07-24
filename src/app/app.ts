5729f406fe911461b7e28f764773634aa30a9672
Galingdu <henglanhzaa09.12.2021@gmail.com>
2025-07-24 16:38:57 +0700

    header and slider

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,19 @@ import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddToCard } from './add-to-card/add-to-card';
+import { Header } from "./header/header";
+import { Slider } from './slider/slider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,AddToCard],
+  imports: [RouterOutlet, CommonModule, AddToCard, Header,Slider],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   products:any = [
   {
+    id:1,
     name: "Air Zoom Pegasus",
     price: 129.99,
     description: "A comfortable, responsive running shoe built for everyday miles.",
@@ -19,13 +22,7 @@ export class App {
     rating: 4.5
   },
   {
-    name: "Adidas Ultraboost Light",
-    price: 159.99,
-    description: "Lightweight cushioning and energy return for a smooth run.",
-    image: "https://freepngimg.com/thumb/shoes/27428-5-nike-shoes-transparent-background.png",
-    rating: 4.8
-  },
-  {
+    id:3,
     name: "Converse Chuck Taylor All Star",
     price: 59.99,
     description: "An iconic canvas sneaker for casual everyday style.",
@@ -33,6 +30,7 @@ export class App {
     rating: 4.5
   },
   {
+    id:4,
     name: "Puma RS-X³ Puzzle",
     price: 110.00,
     description: "Bold, retro-inspired sneaker with exaggerated design elements.",
@@ -40,6 +38,37 @@ export class App {
     rating: 4.4
   },
   {
+    id:5,
+    name: "New Balance 550",
+    price: 109.95,
+    description: "A basketball-inspired lifestyle shoe with vintage flair.",
+    image: "https://parspng.com/wp-content/uploads/2023/02/shoespng.parspng.com-2-600x600.png",
+    rating: 4.6
+  },{
+    id:2,
+    name: "Adidas Ultraboost Light",
+    price: 159.99,
+    description: "Lightweight cushioning and energy return for a smooth run.",
+    image: "https://freepngimg.com/thumb/shoes/27428-5-nike-shoes-transparent-background.png",
+    rating: 4.8
+  },
+  {
+    id:3,
+    name: "Converse Chuck Taylor All Star",
+    price: 59.99,
+    description: "An iconic canvas sneaker for casual everyday style.",
+    image: "https://parspng.com/wp-content/uploads/2023/02/shoespng.parspng.com-11-600x600.png",
+    rating: 4.5
+  },
+  {
+    id:1,
+    name: "Air Zoom Pegasus",
+    price: 129.99,
+    description: "A comfortable, responsive running shoe built for everyday miles.",
+    image: "https://www.pngall.com/wp-content/uploads/13/Nike-Shoes-PNG-Cutout.png",
+    rating: 4.5
+  },{
+    id:5,
     name: "New Balance 550",
     price: 109.95,
     description: "A basketball-inspired lifestyle shoe with vintage flair.",
