010b4cecd03553538c420a32dafa8c17d63d1952
Galingdu <henglanhzaa09.12.2021@gmail.com>
2025-08-18 00:01:41 +0700

    8/18/2025

--- src/app/pay-with-qr/pay-with-qr.ts
+++ src/app/pay-with-qr/pay-with-qr.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Products } from '../Service/products';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-pay-with-qr',
-  imports: [RouterLink],
+  imports: [RouterLink,CurrencyPipe],
   templateUrl: './pay-with-qr.html',
   styleUrl: './pay-with-qr.css'
 })
